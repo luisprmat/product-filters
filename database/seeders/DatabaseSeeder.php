0279a8ca9af7599a66a9bfd13e40d19cc5a973ca
Luis Parrado <luisprmat@gmail.com>
2023-05-30 18:43:27 -0500

    Add models, factories, migrations and seed db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Manufacturer;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(5)->create();
+        Manufacturer::factory(5)->create();
+        Product::factory(20)->create();
     }
 }
