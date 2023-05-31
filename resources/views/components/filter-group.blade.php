9452f35e31ff3b34715528510292f290dd10a9c9
Luis Parrado <luisprmat@gmail.com>
2023-05-31 12:13:36 -0500

    Show count for category and manufacturer filters

--- resources/views/components/filter-group.blade.php
+++ resources/views/components/filter-group.blade.php
@@ -20,13 +20,13 @@
     },
     get results() {
         return this.options.filter((val, index) => {
-        return index >= this.startIndex && index <= this.endIndex
+            return index >= this.startIndex && index <= this.endIndex
         })
     }
 }">
     <h3 class="mt-2 mb-1 text-xl">
         <button @click="open = !open" class="flex w-full items-center justify-between text-left">
-            {{ $title }}
+            <div>{{ $title }} (<span x-text="options.length"></span>)</div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 transform duration-300" :class="{'rotate-180': open}">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
             </svg>
