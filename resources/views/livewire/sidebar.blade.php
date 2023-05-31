a77379c13c80f0a8c206865497b430d9fc6f0998
Luis Parrado <luisprmat@gmail.com>
2023-05-31 00:22:34 -0500

    Collapse categories and prices

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -1,30 +1,60 @@
 <div class="col-lg-3 mb-4">
     <h1 class="mt-4 text-4xl">Filtros</h1>
 
-    <h3 class="mt-2 mb-1 text-3xl">Precio</h3>
-    @foreach($prices as $index => $price)
-        <div>
-            <input type="checkbox" id="price{{ $index }}" value="{{ $index }}" wire:model="selected.prices">
-            <label for="price{{ $index }}">
-                {{ $price['name'] }} ({{ $price['products_count'] }})
-            </label>
+    <div x-data="{ open: true }">
+        <h3 class="mt-2 mb-1 text-2xl">
+            <button @click="open = !open" class="flex w-full items-center justify-between text-left">
+                Precio
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 transform duration-300" :class="{'rotate-180': open}">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                </svg>
+            </button>
+        </h3>
+        <div x-show="open"
+            x-transition.scale.origin.top
+            x-transition:enter.duration.500ms
+            x-transition:leave.duration.500ms
+        >
+            @foreach($prices as $index => $price)
+                <div>
+                    <input type="checkbox" id="price{{ $index }}" value="{{ $index }}" wire:model="selected.prices">
+                    <label for="price{{ $index }}">
+                        {{ $price['name'] }} ({{ $price['products_count'] }})
+                    </label>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 
-    <h3 class="mt-2 mb-1 text-3xl">Categorías</h3>
-    @foreach($categories as $index => $category)
-        <div>
-            <input type="checkbox" id="category{{ $index }}" value="{{ $category->id }}" wire:model="selected.categories">
-            <label for="category{{ $index }}">
-                {{ $category['name'] }} ({{ $category['products_count'] }})
-            </label>
+    <div x-data="{ open: true }">
+        <h3 class="mt-2 mb-1 text-2xl">
+            <button @click="open = !open" class="flex w-full items-center justify-between text-left">
+                Categorías
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 transform duration-300" :class="{'rotate-180': open}">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                </svg>
+            </button>
+        </h3>
+        <div x-show="open"
+            x-transition.scale.origin.top
+            x-transition:enter.duration.300ms
+            x-transition:leave.duration.300ms
+        >
+            @foreach($categories as $index => $category)
+                <div>
+                    <input type="checkbox" id="category{{ $index }}" value="{{ $category['id'] }}" wire:model="selected.categories">
+                    <label for="category{{ $index }}">
+                        {{ $category['name'] }} ({{ $category['products_count'] }})
+                    </label>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 
     <h3 class="mt-2 mb-1 text-3xl">Fabricantes</h3>
     @foreach($manufacturers as $index => $manufacturer)
         <div>
-            <input type="checkbox" id="manufacturer{{ $index }}" value="{{ $manufacturer->id }}" wire:model="selected.manufacturers">
+            <input type="checkbox" id="manufacturer{{ $index }}" value="{{ $manufacturer['id'] }}" wire:model="selected.manufacturers">
             <label for="manufacturer{{ $index }}">
                 {{ $manufacturer['name'] }} ({{ $manufacturer['products_count'] }})
             </label>
